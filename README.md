cf5634c6d35935340e376e851585dc1584796942
SeyyedKhandon <SeyyedKhandon@gmail.com>
2020-10-30 20:21:10 +0330

    feat: add extensions.josn generator

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# vscode-generate-readme-for-your-extentions
+# vscode-generate-readme-for-your-extension-packs
 
- generate packlist.md which contains title/description/url of youre installed vscode extentions, so that you can easily use this in youre vscode extention pack like below examples:
+generate packlist.md which contains title/description/url of youre installed vscode extentions, so that you can easily use this in youre vscode extention pack like below examples:
 
- https://github.com/SeyyedKhandon/vscode-web-developer-experience/blob/main/README.md
+https://github.com/SeyyedKhandon/vscode-web-developer-experience/blob/main/README.md
--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const cheerio = require("cheerio");
 
 const baseURL = "https://marketplace.visualstudio.com";
 const searchURL = "/items?itemName=";
-const extentions = execSync("code --list-extensions")
+const extensions = execSync("code --list-extensions")
   .toString()
   .split("\n")
   .filter((e) => e && e !== "SeyyedKhandon.vscode-web-developer-experience");
-console.log("Extentions :\n", extentions);
-const extentions_url = extentions.map((ext) => baseURL + searchURL + ext);
+const extensionPack = `[\n${extensions.map((i) => `"${i}"`).join(",\n")}\n]`;
+const extensions_url = extensions.map((ext) => baseURL + searchURL + ext);
 
-const getExtentionInfo = async (url) => {
+const getExtensionInfo = async (url) => {
   const html = await rp(url);
   const title = cheerio(".ux-item-name", html).text();
   const description = cheerio(".ux-item-shortdesc", html).text();
   return { title, description, url };
 };
 const convertExtInfoToMarkDownFormat = async (url, index) => {
-  const info = await getExtentionInfo(url);
+  const info = await getExtensionInfo(url);
   return `- ${index + 1}- [${info.title}](${info.url}) - ${info.description}`;
 };
 const generateReadMe = async () => {
   const results = await Promise.all(
-    extentions_url.map(async (url, index) => {
+    extensions_url.map(async (url, index) => {
       try {
         return await convertExtInfoToMarkDownFormat(url, index);
       } catch (error) {
@@ -33,9 +33,21 @@ const generateReadMe = async () => {
     })
   );
   fs.writeFileSync("./packlist.md", results.join("\r\n\n"));
-  console.log("packlist.md has been generated", results);
+  // console.log("packlist.md has been generated", results);
   return results;
 };
-console.log("Fetching extentions informations...");
+
+// console.log("Extensions:\n", extensions);
+console.log("*** Generating extensions.json:\n");
+fs.writeFileSync("./extensions.json", extensionPack);
+console.log("*** Extensions.json Created successfully.\n");
+console.log("     Now you can copy all of it and update your");
+console.log("     vscode extentions pack's package.json's");
+console.log("     'extensionPack' property\n\n");
+
+console.log("Fetching extensions informations...");
 console.log("Please wait...");
 generateReadMe();
+console.log("*** packlist/md Created successfully.\n");
+console.log("     Now you can copy all of it and update your");
+console.log("     vscode extentions pack's 'Readme.md'\n");
